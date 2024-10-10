6446f96d3e9ec26b52d96b42d9d2d4b2a1141070
alexey-gm <gaidaialexey0502@gmail.com>
2024-10-10 12:45:47 +0700

    add app skeleton

--- app/src/main/java/com/example/mycityapp/MainActivity.kt
+++ app/src/main/java/com/example/mycityapp/MainActivity.kt
@@ -7,11 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.mycityapp.ui.theme.MyCityAppTheme
+import com.example.compose.MyCityAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +19,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyCityAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MyCityApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyCityAppPreview() {
+    MyCityAppTheme {
+        MyCityApp()
     }
 }
---
+++ app/src/main/java/com/example/mycityapp/MyCityApp.kt
@@ -0,0 +1,12 @@
+package com.example.mycityapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MyCityApp(
+    modifier: Modifier = Modifier
+) {
+
+}
+
